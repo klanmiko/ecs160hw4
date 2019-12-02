85f2ae7fd3f4149dc325d04e0bfa2bd9b4a8c3f1
Kaelan Mikowicz <eternity01033@gmail.com>
2019-12-02 12:11:47 -0800

    final submission

--- maxTweeter.c
+++ maxTweeter.c
@@ -3,13 +3,66 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include "parser.h"
+typedef struct {
+    char** tweets;
+    size_t length;
+} tweet_vector;
+
+typedef struct {
+    size_t numCols;
+    size_t nameIndex;
+    bool* columnsQuoted;
+} header_info;
+
+tweet_vector getTweets(FILE* fPtr);
+
+void checkLineLength(size_t len);
+bool* getQuotation(char* line);
+
+typedef struct {
+    char* name;
+    unsigned int count;
+} tweet_count;
+
+typedef struct {
+    tweet_count* tweeters;
+    size_t length;
+} collected_tweets;
+
+collected_tweets collectTweets(char** rows, size_t n_rows);
+
+#define INVALID_ERROR "Invalid Input Format"
+
+/* Prints that there is an invalid error and exits the program
+  */
+void die(char* reason) {
+    #ifdef DEBUG
+    printf("%s", reason);
+    #endif
+    printf(INVALID_ERROR);
+    exit(EXIT_FAILURE);
+}
+
+/* getFile opens the filePath as a file pointer
+  if the file cannot be opened, this function will exit the program
+  the return value cannot be null
+*/
+
+FILE* getFile(char* filePath) {
+  FILE* filePtr = fopen(filePath, "r");
+
+  if (filePtr == NULL) {
+      die("Could not open file.");
+  }
+
+  return filePtr;
+}
+
+
 
 #define STRIP_NEWLINE(s) { size_t l = strlen(s); \
     if(l > 0 && s[l - 1] == '\n') s[l - 1] = '\0'; \
     if(l > 1 && s[l - 2] == '\r') s[l - 2] = '\0'; }
-
-#include "util.h"
 
 char* readName(char* line, size_t numCols, size_t nameIndex, bool nameQuoted);
 header_info readHeaderQuick(char* line);
@@ -278,68 +331,131 @@ char* readName(char* line, size_t numCols, size_t nameIndex, bool nameQuoted) {
     return name;
 }
 
-/*
-header_info readHeader(char* line, bool nameQuoted) {
-    size_t numCols = 0, i = 0, nameIndex = 0;
-    char c;
-    char* currCol = (char*)malloc(sizeof(char));
-    size_t currColI = 0, currCol_size = 1;
-    bool seenNameCol = false; // Check in case there are two "name" columns
+/* Returns the tweet_count (which contains the name and tweet count) 
+ with the greater tweet count. Used as a comparator function for qsort()
+*/
+int compareTweet(const void* a, const void* b) {
+  const tweet_count *first = a, *second = b;
+  return second->count - first->count;
+}
 
-    while ((c = line[i]) != '\0') {
-        if (c == ',') {
-            // If you reach a comma, check if the current column is a "name" column
-            currCol[currColI] = '\0';
 
-            if (strcmp("\"name\"", currCol) == 0) {
+int name_sort(const void *a, const void *b)
+{
+    return strcmp(*(const char **)a, *(const char **)b);
+}
 
-                // If you already saw a name column, die
-                if (seenNameCol) {
-                    die();
+collected_tweets collectTweets(char **names, size_t n_rows)
+{
+    // no tweeters
+    if (n_rows == 0)
+    {
+        return (collected_tweets){NULL, 0};
+    }
+
+    // the tweet at this index in tweeters is uninitialized
+    size_t index = 0, t_size = 1;
+    tweet_count *tweeters = (tweet_count *)malloc(sizeof(tweet_count));
+    if(tweeters == NULL) {
+        die("Malloc failed to allocate tweeters");
+    }
+
+    qsort(names, n_rows, sizeof(char *), name_sort);
+    char *prev_name = NULL;
+    size_t prev_count = 1;
+
+    // iterate over the rows
+    for (size_t i = 0; i < n_rows; i++)
+    {
+        char *name = names[i];
+        // if a new name is seen, record count to array
+        if (prev_name != NULL)
+        {
+            if (strcmp(name, prev_name) != 0)
+            {
+                tweet_count *t = &(tweeters[index++]);
+                t->count = prev_count;
+
+                size_t l = strlen(prev_name) + 1;
+                t->name = (char*)malloc(sizeof(char) * l);
+
+                if(t->name == NULL) die("Failed to allocate space for name of tweet\n");
+
+                strncpy(t->name, prev_name, l);
+
+                // if the next free index = the size, resize the array to 2n + 1
+                if (index == t_size)
+                {
+                    tweeters = (tweet_count *)realloc(tweeters, (t_size * 2 + 1) * sizeof(tweet_count));
+                    t_size = t_size * 2 + 1;
+
+                    if (tweeters == NULL)
+                    {
+                        die("Failed to reallocate space for tweeters\n");
+                    }
                 }
 
-                nameIndex = numCols;
-                seenNameCol = true;
+                prev_count = 1;
             }
-
-            numCols++;
-            free(currCol);
-            currColI = 0, currCol_size = 0;
-            currCol = (char*)malloc(sizeof(char));
-        } else {
-            // Otherwise, keep adding characters to the current column
-            currCol = (char*)realloc(currCol, (currCol_size + 1)*sizeof(char));
-            currCol[currColI] = c;
-            currColI++;
-            currCol_size++;
-        }
-        
-        i++;
-    }
-
-    // Checks if the last column is a "name" column
-    currCol[currColI] = '\0';
-
-    if (strcmp("\"name\"", currCol) == 0) {
-
-        if (seenNameCol) {
-            die();
+            else if (strcmp(name, prev_name) == 0)
+            {
+                prev_count++;
+            }
         }
 
-        nameIndex = numCols;
-        seenNameCol = true;
+        prev_name = name;
     }
 
-    numCols++;
-    free(currCol);
+    tweet_count *t = &(tweeters[index++]);
+    t->count = prev_count;
 
-    // If there are no columns in the header or the name column doesn't exist, die
-    if (numCols == 0 || !seenNameCol) {
-        die();
-    }
+    size_t l = strlen(prev_name) + 1;
+    t->name = (char*)malloc(sizeof(char) * l);
+    
+    if(t->name == NULL) die("Failed to allocate space for name of tweet\n");
 
-    // Return a struct containing the number of columns and index of name col
-    header_info info = {numCols, nameIndex};
-    return info;
+    strncpy(t->name, prev_name, l);
+
+    return (collected_tweets){tweeters, index};
 }
-*/
+
+int main(int argc, char* argv[]) {
+    FILE* csvFile;
+    // Checks for two command line arguments
+    if(argc == 2) {
+        csvFile = getFile(argv[1]);
+    } else {
+        die("Usage: main <csvFile>");
+    }
+
+    // Creates a list of tweets containing rows of tweets
+    tweet_vector rows = getTweets(csvFile);
+    collected_tweets tweets = collectTweets(rows.tweets, rows.length);
+
+    // Free the space allocated for rows of tweets
+    for(size_t i = 0; i < rows.length; i++) {
+      free(rows.tweets[i]);
+    }
+    free(rows.tweets);
+    
+    // Sorts tweeters according to their tweet count
+    qsort(tweets.tweeters, tweets.length, sizeof(tweet_count), compareTweet);
+
+    int top = tweets.length < 10 ? tweets.length : 10;
+
+    // Print out the top tweeters and their tweet counts
+    for(size_t i = 0; i < top; i++) {
+      tweet_count t = tweets.tweeters[i];
+      printf("%s: %d\n", t.name, t.count);
+    }
+
+    for(size_t i = 0; i < tweets.length; i++) {
+      free(tweets.tweeters[i].name);
+    }
+
+    // Free the tweeters and close file
+    if(tweets.tweeters != NULL) free(tweets.tweeters);
+    fclose(csvFile);
+
+    return 0;
+}
